7663ac9e653f7772ccbff95862658bd6a2fc131e
Klovak <stefanklovak@yandex.ru>
2025-01-15 23:10:26 +0300

    f

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,5 @@
-FROM python:latest
+FROM apache/airflow:2.6.0
 
-# Установим рабочую директорию
-WORKDIR /app
+COPY requirements.txt /opt/airflow/
 
-# Копируем файлы проекта (замените на ваши файлы)
-COPY . /app/
-
-# Копируем requirements.txt
-COPY requirements.txt /app/
-
-# Установка зависимостей
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Команда для запуска вашего бота (замените на команду запуска вашего скрипта)
-CMD ["python", "pbot.py"]
+RUN pip install --no-cache-dir -r /opt/airflow/requirements.txt
--- dags/bot_dag.py
+++ dags/bot_dag.py
@@ -1,32 +1,61 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
-import subprocess
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import logging
+import requests
+import telebot
+import re
+import os
 
-# Функция для запуска вашего Jupyter Notebook
-def run_bot_notebook():
-    #subprocess.run(["jupyter", "nbconvert", "--to", "script", "/path/to/your/notebook/pbot.ipynb"])
-    subprocess.run(["python", "pbot.py"])
+def run_():
+    logging.info("Функция run_ запущена")
+    try:
+        url = "https://quotes15.p.rapidapi.com/quotes/random/"
+        querystring = {"language_code": "ru"}
+        headers = {
+            "x-rapidapi-key": os.getenv("RAPIDAPI_KEY"),
+            "x-rapidapi-host": "quotes15.p.rapidapi.com"
+        }
+        TOKEN = os.environ.get('BOT_TOKEN')
+        CHANNEL_ID = '@TestPost96'
+        bot = telebot.TeleBot(TOKEN)
 
-# Определение DAG
+        while True:
+            try:
+                response = requests.get(url, headers=headers, params=querystring)
+                new_post = response.json()
+                post_content = new_post['content']
+                if len(post_content) < 100 or re.search(r'\bроссия\b', post_content, re.IGNORECASE):
+                    break
+            except Exception as e:
+                logging.error(f"Ошибка при запросе: {e}")
+                continue
+
+        bot.send_message(CHANNEL_ID, post_content)
+        logging.info(f"Сообщение отправлено в канал: {post_content}")
+
+    except Exception as e:
+        logging.error(f"Ошибка при выполнении функции: {e}")
+
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 10, 23),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2025, 1, 15),
 }
 
 dag = DAG(
-    'telegram_bot_dag',
+    dag_id='bot_dag',
     default_args=default_args,
     description='DAG for running Telegram bot',
-    schedule_interval = '* * * * *',
-    #schedule_interval='0 9,11,13,15,17,19,21, * * *',
+    schedule_interval='16 8,12,17 * * *',  # Запуск в 11:16, 15:16 и 20:16
+    catchup=False,
 )
 
 run_bot = PythonOperator(
     task_id='run_bot_notebook',
-    python_callable=run_bot_notebook,
+    python_callable=run_,
     dag=dag,
 )
+
+run_bot
